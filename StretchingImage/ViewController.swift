f5ac7304d2011ec50060c5abf49c53df496b27b6
Mikhail Andreev <qr.stronger@gmail.com>
2023-07-21 19:52:03 +0300

    Done seventh task

--- StretchingImage/ViewController.swift
+++ StretchingImage/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+	private let imageHeight: CGFloat = 270
 
-	override func viewDidLoad() {
+	private lazy var imageView: UIImageView = {
+		let image = UIImage(named: "billy")
+		let imageView = UIImageView(image: image)
+		imageView.frame.size = .init(width: view.frame.width, height: imageHeight)
+		imageView.frame.origin = .zero
+		imageView.contentMode = .scaleAspectFill
+		imageView.clipsToBounds = true
+		return imageView
+	}()
+
+	private lazy var scrollView: UIScrollView = {
+		let scrollView = UIScrollView(frame: view.frame)
+		scrollView.contentSize.height = view.frame.height + imageHeight
+		scrollView.delegate = self
+		return scrollView
+	}()
+
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		view.backgroundColor = .systemBackground
+		view.addSubview(scrollView)
+		scrollView.addSubview(imageView)
 	}
-
-
 }
 
+extension ViewController: UIScrollViewDelegate
+{
+	public func scrollViewDidScroll(_ scrollView: UIScrollView)
+	{
+		let offset = scrollView.contentOffset.y
+		if offset <= .zero
+		{
+			imageView.frame.origin.y = offset
+			imageView.frame.size.height = imageHeight - offset
+		}
+		scrollView.verticalScrollIndicatorInsets.top = imageHeight - offset - view.layoutMargins.top
+	}
+}
